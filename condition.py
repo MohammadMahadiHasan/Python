3883cf04b190b4b5176fd742647a498690f6e682
Mohammad Mahadi Hasan <104937244+MohammadMahadiHasan@users.noreply.github.com>
2022-12-24 21:30:14 +0600

    condition added

---
+++ condition.py
@@ -0,0 +1,22 @@
+n=1
+
+if n > 0: 
+    print("n is positive")
+else:
+    print("n is negative")
+    
+m=input("Number:")
+if m is not None:
+    print("Number"+m)
+else:
+    print("Number is not specified")
+
+b=int(input("Number: "))
+
+if b>0:
+    print("n is positive")
+elif b<0:
+    print("n is negative")
+else:
+    print("n is zero")    
+  
